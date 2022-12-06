068c08d66f9e54bd672c1c51b83ae7af2c5e17e7
HannaBerggren <hannajberggren@gmail.com>
2022-12-06 18:53:51 +0000

    Add function to see who wins and to end game after 10 moves

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,36 @@ const game = () => {
     let playerScore = 0;
     let computerScore = 0;
     let moves = 0;
+    
+/**
+ * Function to play the game
+ */
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('paper');
+const scissorsButton = document.querySelector('scissors');
+const playerOptions = [rockButton, paperButton, scissorsButton];
+const computerOptions = ['rock', 'paper', 'scissors'];
+
+/**
+ * Function to start play
+ */
+playerOptions.forEach(option => {
+    option.addEventListener('click', function() {
+
+        const movesLeft = document.querySelector('.movesLeft'); moves++;
+        movesLeft.innerText = `Moves Left: ${10-moves}`;
+
+        const choiseNumber = Math.floor(Math.random()*3);
+        const computerChoise = computerOptions[choiseNumber];
+
+ /**
+ * Function to see who wins
+ */
+        winner(this.innerText, computerChoice);
+
+        if(moves === 10) {
+            gameOver(playerOptions, movesLeft);
+        }
+    })
+});
 };  
